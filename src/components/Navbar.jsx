e189d6bb1352c735f0c693d5954e8e9d45c1f75e
ujwalamar314-cell <ujwalamar314-cell@users.noreply.github.com>
2026-07-29 20:32:27 +0530

    commit and push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className='bg-mauve-800 text-white text-2xl flex w-full items-center pl-5 md:pl-20 h-17'>
       <div className="font-bold flex items-center">
         <span className='mr-2'>
-          <img className='w-10' src="/public/logo.png" alt="" />
+          <img className='w-10' src="/logo.png" alt="" />
         </span>
         <span>My</span>
         <span className='text-red-400'>PsW</span>
