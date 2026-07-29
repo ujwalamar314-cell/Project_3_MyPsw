9adac28e982d83b26924f247c483a90bb6363a4c
ujwalamar314-cell <ujwalamar314-cell@users.noreply.github.com>
2026-07-29 20:37:47 +0530

    commit and push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+const logoUrl = new URL('/logo.png', import.meta.url).href
+
 const Navbar = () => {
   return (
-    <div className='bg-mauve-800 text-white text-2xl flex w-full items-center pl-5 md:pl-20 h-17'>
+    <div className='bg-mauve-800 text-white text-2xl flex w-full items-center pl-5 md:pl-20 h-16'>
       <div className="font-bold flex items-center">
         <span className='mr-2'>
-          <img className='w-10' src="/logo.png" alt="" />
+          <img className='w-10 h-10 object-contain' src={logoUrl} alt="MyPsW logo" />
         </span>
         <span>My</span>
         <span className='text-red-400'>PsW</span>
